beacb1a40960c91e520fd3f9b697e5498571da63
Derek <513407656@qq.com>
2017-05-24 19:14:42 +0800

    Update slideToDo.js

--- slideToDo.js
+++ slideToDo.js
@@ -1,12 +1,12 @@
 (function(){
 
-	window.requestAnimationFrame = window.requestAnimationFrame
+window.requestAnimationFrame = window.requestAnimationFrame
     || window.mozRequestAnimationFrame
     || window.webkitRequestAnimationFrame
     || window.msRequestAnimationFrame
     || function(f){return setTimeout(f, 1000/60)} // simulate calling code 60 
  
-	window.cancelAnimationFrame = window.cancelAnimationFrame
+window.cancelAnimationFrame = window.cancelAnimationFrame
     || window.mozCancelAnimationFrame
     || function(requestID){clearTimeout(requestID)} //fall back
 
